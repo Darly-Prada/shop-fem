053c1752714e83d46d4dd6553e8aefcc2158a552
clear <darly.prada26@gmail.com>
2024-12-02 17:07:52 -0500

    proyecto ecommerce 1 preentrega

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from './NavBar/NavBar'
+import ItemListContainer from './ItemListContainer/ItemListContainer'
+import './App.css'
+
+
+function App() {
+  
+  return (
+  <div>
+  <NavBar />
+  <ItemListContainer saludo={"Bienvenid@s a ShopFem"}/>
+  </div>
+
+  )
+}
+export default App
---
+++ src/NavBar/CartWidget.jsx
@@ -0,0 +1,10 @@
+import { MdShoppingCart } from "react-icons/md";
+const CartWidget = () => {
+  return (
+    <div className="widget">
+        <MdShoppingCart />
+        <span>1</span>
+    </div>
+  )
+}
+export default CartWidget
